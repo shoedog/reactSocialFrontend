031c95e214238f0d140e4f97597dcab7a2e16ba7
Alex Rappa <rappab@oregonstate.edu>
2016-10-15 15:20:25 -0500

    Alex: Added a 404 Page to routes

---
+++ src/components/Page404/Page404.jsx
@@ -0,0 +1,16 @@
+import React, { Component, PropTypes } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect }            from 'react-redux';
+require("!style!css!./Page404.css");
+
+class Page404 extends Component {
+	render() {
+		return (
+			<div className="page404">
+				<h1>404 - Page Not Found</h1>
+			</div>
+		);
+	}
+}
+
+export default Page404
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
-
 import LaunchPageHome from './components/LaunchPage_Home/LaunchPageHome'
+import Page404 from './components/Page404/Page404'
 import App from './Containers/App';
 
 
 export default (
-	<Route name="app" component={App} path="/">
+	<Route name="app" path="/" component={App}>
 		<Route name="home" path="home" component={LaunchPageHome} />
+		<Route name="404" path="*" component={Page404} />
 	</Route>
 );
