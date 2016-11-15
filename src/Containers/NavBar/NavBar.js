ac80ff83710ee2c2551d068ab4a44064bfed52bc
Andrew <andrewbrown600@gmail.com>
2016-11-14 20:28:12 -0800

    when you sign in, you get a menu with profile, signout options

--- src/Containers/NavBar/NavBar.js
+++ src/Containers/NavBar/NavBar.js
@@ -2,20 +2,71 @@ import React, { PropTypes } from 'react';
 import { IndexLink, Link } from 'react-router';
 import { connect } from 'react-redux';
 import {AppBar, FlatButton, IconButton} from 'material-ui';
+import SmallMenu from './SmallMenu';
 
-const NavBar = () => (
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleResize = this.handleResize.bind(this);
+    this.state = {
+     windowWidth: 1000
+    };
+  }
 
-  <AppBar
-    title = {<FlatButton label="Moonwalk" href="/" />}
-    iconElementRight= {
-      <div>
-        <FlatButton label="About" href="/about" />
-        <FlatButton label="Contact" href="/contact"  />
-        <FlatButton label="Stream" href="/stream"  />
-        <IconButton iconClassName="muidocs-icon-custom-github" href="https://github.com/andrew310/cs419-frontend" tooltip="Frontend" />
-     </div>
+
+  // add listeners
+  componentDidMount () {
+    window.addEventListener('resize', this.handleResize);
+  }
+
+  componentWillUnmount () {
+    window.removeEventListener('resize', this.handleResize);
+  }
+
+  // keep track of window size so we can restyle AppBar
+  handleResize (e) {
+    this.setState({windowWidth: global.innerWidth});
+  }
+
+  // logo takes user to home page
+  handleLogoTouchTap () {
+    window.location.href="/";
+  }
+
+  // whether to show buttons or small menu
+  getWindow () {
+    if(this.state.windowWidth > 600 && !sessionStorage.getItem('username')) {
+      return (
+        <div>
+          <FlatButton label="About" href="/about" />
+          <FlatButton label="Contact" href="/contact"  />
+          <FlatButton label="Stream" href="/stream"  />
+          <IconButton iconClassName="muidocs-icon-custom-github" href="https://github.com/andrew310/cs419-frontend" tooltip="Frontend" />
+       </div>
+    );} else {
+      return(
+        <SmallMenu />
+      );
     }
-  />
-);
+  }
+
+  render () {
+    const getWindow = this.getWindow();
+
+    return (
+      <AppBar
+        title = {<FlatButton label="Moonwalk" href="/" />}
+        iconElementRight= {
+          <div>
+          {getWindow}
+          </div>
+        }
+      />
+    );
+  } // end of render
+} // end of class
+
+
+
 
 export default NavBar;
---
+++ src/Containers/NavBar/SmallMenu.js
@@ -0,0 +1,64 @@
+// app/components/SmallMenu.js
+'use strict';
+import React from 'react';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import IconMenu from 'material-ui/IconMenu';
+import IconButton from 'material-ui/IconButton';
+import MenuItem from 'material-ui/MenuItem';
+import Divider from 'material-ui/Divider';
+import Subheader from 'material-ui/Subheader';
+
+class SmallMenu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.auth = this.props.AuthStore;
+    this.profile = this.props.ProfileStore;
+    this.handleSignout = this.handleSignout.bind(this);
+    this.getUser = this.getUser.bind(this);
+  }
+
+  handleSignout() {
+    this.auth.signout();
+  }
+
+  /**
+   * Displays different menu according to whether user is logged in.
+   */
+
+  getUser() {
+    if (sessionStorage.getItem('username') && sessionStorage.getItem('token')) {
+      return (
+        <div>
+        <Subheader> {sessionStorage.getItem('username')} </Subheader>
+        <Divider />
+        <MenuItem primaryText="Profile" />
+        <MenuItem primaryText="Sign out" onClick={this.handleSignout}/>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+        <MenuItem primaryText="Sign In" />
+        <MenuItem primaryText="Sign Up" />
+        </div>
+      );
+    }
+  }
+
+  render(){
+    const getUser = this.getUser();
+    return(
+      <IconMenu
+        iconButtonElement={
+          <IconButton><MoreVertIcon /></IconButton>
+        }
+        targetOrigin={{horizontal: 'right', vertical: 'top'}}
+        anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+        >
+        {getUser}
+      </IconMenu>
+    );
+  }
+}
+
+export default SmallMenu;
