a53b69c8052e5b09da4d5b55bbf09aae19d44df1
shoedog <wesley.jinks@gmail.com>
2016-12-02 23:45:04 -0700

    trending

--- src/components/Trends/availableTrends.js
+++ src/components/Trends/availableTrends.js
@@ -7,6 +7,8 @@ import RaisedButton from 'material-ui/RaisedButton';
 import FlatButton from 'material-ui/FlatButton';
 import FaTwitter from 'react-icons/lib/fa/twitter';
 import StreamItem from '../StreamItem/StreamItem';
+import Subheader from 'material-ui/Subheader';
+import {List, ListItem, makeSelectable} from 'material-ui/List';
 import s from './trends.css';
 import Snackbar from 'material-ui/Snackbar';
 import { fetchJson } from '../../utils/lib/fetchUtils';
@@ -20,13 +22,19 @@ class AvailableTrends extends Component {
             open: false,
             dopen: false,
             searchData: null,
+            trendItems: null,
         };
         this.getTrending = this.getTrending.bind(this);
         this.handleClick = this.handleClick.bind(this);
+        this.viewTweets = this.viewTweets.bind(this);
     }
 
     handleClick(e) {
       this.getTrending(e);
+    }
+
+    handleTrend(e) {
+        this.viewTweets(e);
     }
 
     getId = (e) => {
@@ -50,102 +58,128 @@ class AvailableTrends extends Component {
         .then((res) => {
             console.log('TRENDS');
             console.log(res);
-            //this.setState({availableTrends: res});
+            this.setState({trendItems: res});
         })
     };
- // viewTweets = (term) => {
- //     let options = { method: 'GET'};
- //     return fetchJson(`http://localhost:5000/social/stream/${term}`,
- //         {options})
- //     .then((res) => {
- //         console.log(res);
- //         this.setState({searchData: res});
- //         this.setState({dopen: !this.state.dopen});
- //     })
- // };
- // getText(tweet) {
- //     if ( tweet.extended_tweet ){
- //         return tweet.extended_tweet.full_text;
- //     } else if ( tweet.retweeted_status ) {
- //         if ( tweet.retweeted_status.extended_tweet ){
- //             return tweet.retweeted_status.extended_tweet.full_text;
- //         }
- //         return tweet.retweeted_status.text;
- //     }
- //     return tweet.text;
- // }
- // getImgs(tweet) {
- //     if ( tweet.extended_entities) {
- //         let imgs = tweet.extended_entities.media.filter((media) => {
- //             if ( media.type === 'photo'){
- //                 return media.media_url;
- //             }
- //         });
- //         return imgs;
- //     } else if (tweet.entitites) {
- //         let imgs = tweet.entities.media.filter((media) => {
- //             if ( media.type === 'photo') {
- //                 return media.media_url;
- //             }
- //         });
- //         return imgs;
- //     }
- // }
- // getGif(tweet) {
- //     if (tweet.extended_entities) {
- //         let gifs = tweet.extended_entities.media.filter((media) => {
- //             if( media.type === 'animated_gif'){
- //                 return media.video_info.variants[0].url;
- //             }
- //         });
- //         return gifs;
- //     }
- // }
- //**
 
- //   getDrawer(searchGroup) {
- //       if (searchGroup == null ) {
- //           return (
- //               <div>
- //               </div>
- //           );
- //       } else {
- //           return (
- //               <div>
- //                   {/* Begin Left Drawer */}
- //                   <Drawer open={this.state.dopen} width={400} docked={false} openSecondary={true} onRequestChange={(dopen) => this.setState({dopen})}>
- //                       <GridList cellHeight='auto' style={{
- //                           width: 350,
- //                           display: 'flex',
- //                           justifyContent: 'center',
- //                           marginLeft: '10%',
- //                           marginTop: 40
- //                       }} padding={10} cols={1}>
- //                           {searchGroup.map((tweet) => (
- //                               <GridTile key={tweet.id_str}>
- //                                   <StreamItem
- //                                       tweetId={tweet.id_str}
- //                                       favorited={tweet.favorited}
- //                                       retweeted={tweet.retweeted}
- //                                       retweetCount={tweet.retweet_count}
- //                                       favoriteCount={tweet.favorite_count}
- //                                       friend={tweet.user.screen_name}
- //                                       avatarImg={tweet.user.profile_image_url}
- //                                       textContent={this.getText(tweet)} />
- //                               </GridTile>
- //                           ))}
- //                       </GridList>
- //                   </Drawer>
- //                   {/* End Left Drawer */}
- //               </div>
- //           );
- //       }
- //   }
- //
+  viewTweets = (term) => {
+      let options = { method: 'GET'};
+      return fetchJson(`http://localhost:5000/social/stream/${term}`,
+          {options})
+      .then((res) => {
+          console.log(res);
+          this.setState({searchData: res});
+          this.setState({dopen: !this.state.dopen});
+      })
+  };
+
+  getText(tweet) {
+      if ( tweet.extended_tweet ){
+          return tweet.extended_tweet.full_text;
+      } else if ( tweet.retweeted_status ) {
+          if ( tweet.retweeted_status.extended_tweet ){
+              return tweet.retweeted_status.extended_tweet.full_text;
+          }
+          return tweet.retweeted_status.text;
+      }
+      return tweet.text;
+  }
+  getImgs(tweet) {
+      if ( tweet.extended_entities) {
+          let imgs = tweet.extended_entities.media.filter((media) => {
+              if ( media.type === 'photo'){
+                  return media.media_url;
+              }
+          });
+          return imgs;
+      } else if (tweet.entitites) {
+          let imgs = tweet.entities.media.filter((media) => {
+              if ( media.type === 'photo') {
+                  return media.media_url;
+              }
+          });
+          return imgs;
+      }
+  }
+  getGif(tweet) {
+      if (tweet.extended_entities) {
+          let gifs = tweet.extended_entities.media.filter((media) => {
+              if( media.type === 'animated_gif'){
+                  return media.video_info.variants[0].url;
+              }
+          });
+          return gifs;
+      }
+  }
+
+    getTrends(trendList) {
+        console.log('test');
+        console.log(trendList);
+        if (trendList === null) {
+            return (
+                <div>
+                </div>
+            );
+        } else {
+            return (
+                <ul>
+                    {trendList[0].trends.map((trend) => (
+                            <li>
+                                <FlatButton label={trend.name + ' Tweets: ' + trend.tweet_volume}
+                                onClick={this.handleTrend.bind(this, trend['query'])}/>
+                            </li>
+
+                    ))}
+
+                </ul>
+            )
+        }
+    }
+
+
+    getDrawer(searchGroup) {
+        console.log(searchGroup);
+        if (searchGroup == null ) {
+            return (
+                <div>
+                </div>
+            );
+        } else {
+            return (
+                <div>
+                    {/* Begin Left Drawer */}
+                    <Drawer open={this.state.dopen} width={400} docked={false} openSecondary={true} onRequestChange={(dopen) => this.setState({dopen})}>
+                        <GridList cellHeight='auto' style={{
+                            width: 350,
+                            display: 'flex',
+                            justifyContent: 'center',
+                            marginLeft: '10%',
+                            marginTop: 40
+                        }} padding={10} cols={1}>
+                            {searchGroup.statuses.map((tweet) => (
+                                <GridTile key={tweet.id_str}>
+                                    <StreamItem
+                                        tweetId={tweet.id_str}
+                                        favorited={tweet.favorited}
+                                        retweeted={tweet.retweeted}
+                                        retweetCount={tweet.retweet_count}
+                                        favoriteCount={tweet.favorite_count}
+                                        friend={tweet.user.screen_name}
+                                        avatarImg={tweet.user.profile_image_url}
+                                        textContent={this.getText(tweet)} />
+                                </GridTile>
+                            ))}
+                        </GridList>
+                    </Drawer>
+                    {/* End Left Drawer */}
+                </div>
+            );
+        }
+    }
+
 
     render() {
         let link = "";
-
         return(
             <div>
 
@@ -154,23 +188,24 @@ class AvailableTrends extends Component {
                     <div style={{display: 'flex', }}>
                         <div>
                             {/* Begin Place List */}
+                            <ul >
                             {cities.map((city, handle=fn) => (
-                                <ul key={city.woied}>
-                                    <li> <FlatButton label={city.city} onClick={this.handleClick.bind(this, city['woeid'])} /></li>
-                                </ul>
-                            ))}
 
+                                    <li key={city.woied}> <FlatButton label={city.name} onClick={this.handleClick.bind(this, city['woeid'])} /></li>
+
+                            ))}
+                            </ul>
                             {/* End Place List */}
                         </div>
                         <div>
                             <p>TRENDS</p>
                             {/* Begin Trend List  {this.getTrends(this.state.availableTrends)}*/}
-
+                            {this.getTrends(this.state.trendItems)}
                             {/* End Trend List */}
                         </div>
                     </div>
                     {/* Begin Left Drawer {this.getDrawer(this.state.searchData)}*/}
-
+                    {this.getDrawer(this.state.searchData)}
                     {/* End Left Drawer */}
                 </Paper>
                 {/* END MAIN BLOCK */}
